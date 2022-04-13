9df2d738a804eb06d27093b5d1aa1f5a4cfd70e5
hanyo789 <thinhcuong2001@gmail.com>
2022-04-13 17:55:35 +0700

    add crud idea and edit idea relation with file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { AuthModule } from './modules/v1/auth/auth.module';
 import { UserModule } from './modules/v1/user/user.module';
 import { DepartmentModule } from './modules/v1/department/department.module';
 import { TopicModule } from './modules/v1/topic/topic.module';
+import { IdeaModule } from './modules/v1/idea/idea.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpExceptionFilter } from './shared/http-exception.filter';
 import { APP_FILTER } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, MigrationModule, AuthModule, UserModule, DepartmentModule, TopicModule],
+  imports: [DatabaseModule, MigrationModule, AuthModule, UserModule, DepartmentModule, TopicModule, IdeaModule],
   controllers: [AppController],
   providers: [
     AppService,
