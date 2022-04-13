4c6b00555ead864e4a2b19e37acfdd7f44e38fc2
minhtuan1132 <minhtuan789t@gmail.com>
2022-04-13 22:09:26 +0700

    add upload to s3 service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,14 @@ import { UserModule } from './modules/v1/user/user.module';
 import { DepartmentModule } from './modules/v1/department/department.module';
 import { TopicModule } from './modules/v1/topic/topic.module';
 import { IdeaModule } from './modules/v1/idea/idea.module';
+import { FiledModule } from './modules/v1/file/file.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpExceptionFilter } from './shared/http-exception.filter';
 import { APP_FILTER } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, MigrationModule, AuthModule, UserModule, DepartmentModule, TopicModule, IdeaModule],
+  imports: [DatabaseModule, MigrationModule, AuthModule, UserModule, DepartmentModule, TopicModule, IdeaModule, FiledModule],
   controllers: [AppController],
   providers: [
     AppService,
