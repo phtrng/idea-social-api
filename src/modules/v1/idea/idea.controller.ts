e99081af463c680f56436a0c17bcfa2f0e6c3825
hanyo789 <thinhcuong2001@gmail.com>
2022-04-14 00:59:44 +0700

    add vote and file to idea

--- src/modules/v1/idea/idea.controller.ts
+++ src/modules/v1/idea/idea.controller.ts
@@ -1,15 +1,30 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+  UseInterceptors,
+  UploadedFiles,
+  BadRequestException,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { IdeaService } from './idea.service';
 import { AuthGuard } from 'src/modules/v1/auth/guard/auth.guard';
 import { RolesGuard } from 'src/modules/v1/auth/guard/role.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiConsumes } from '@nestjs/swagger';
 import { IdeaCreateDTO, IdeaUpdateDTO, IdeaListDTO } from './dto/idea.dto';
+import { FileService } from 'src/modules/v1/file/file.service';
 
 @Controller('api/v1/idea')
 @ApiTags('Idea')
 @UseGuards(AuthGuard, RolesGuard)
 export class IdeaController {
-  constructor(private readonly service: IdeaService) {}
+  constructor(private readonly service: IdeaService, private readonly fileService: FileService) {}
   @Get()
   async getMany() {
     return await this.service.getMany();
@@ -23,13 +38,55 @@ export class IdeaController {
     return await this.service.getOne(id);
   }
   @Post()
-  async createOne(@Body() dto: IdeaCreateDTO) {
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'document_id', maxCount: 1 },
+      { name: 'image_id', maxCount: 1 },
+    ]),
+  )
+  @ApiConsumes('multipart/form-data')
+  async createOne(@UploadedFiles() files: { document_id?: any; image_id?: any }, @Body() dto: IdeaCreateDTO) {
+    if (files && (files.image_id || files.document_id)) {
+      let fileArr = [];
+      if (Array.isArray(files.image_id)) fileArr.push(files.image_id[0]);
+      if (Array.isArray(files.document_id)) fileArr.push(files.document_id[0]);
+      let filePromises = fileArr.map(async (e) => {
+        const mime = e.mimetype.split('/')[1];
+        const mines = e.fieldname === 'image_id' ? ['jpeg', 'jpg', 'png', 'gif'] : ['pdf', 'docx', 'doc', 'txt', 'xlsx', 'xls', 'pptx', 'ppt'];
+        const type = e.fieldname === 'image_id' ? 'image' : 'document';
+        if (!mines.includes(mime)) {
+          throw new BadRequestException(e.fieldname + ` is must be an ${type}`);
+        }
+        if (e.size > process.env.MAX_UPLOAD_SIZE) {
+          throw new BadRequestException(e.fieldname + ' is must be smaller than 5Mb');
+        }
+        const data = await this.fileService.createOneFile(e);
+        if (data.id) {
+          if (e.fieldname === 'image_id') {
+            dto.image_id = data.id;
+          } else if (e.fieldname === 'document_id') {
+            dto.document_id = data.id;
+          }
+        }
+      });
+      await Promise.all(filePromises);
+    }
     return await this.service.createOne(dto);
   }
+
   @Put(':id')
   async updateOne(@Param('id') id: number, @Body() dto: IdeaUpdateDTO) {
     return await this.service.updateOne(id, dto);
   }
+  @Post(':id/upvote')
+  upvoteIdea(@Param('id') id: number) {
+    return this.service.upVote(id);
+  }
+  @Post(':id/downvote')
+  downvoteIdea(@Param('id') id: number) {
+    return this.service.downVote(id);
+  }
+
   @Delete(':id')
   async deleteOne(@Param('id') id: number) {
     return await this.service.deleteOne(id);
